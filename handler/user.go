c3f79f858b1e66ce0ddc75ffed4c7cebb03097a4
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2022-06-25 08:12:01 +0700

    first commit

---
+++ handler/user.go
@@ -0,0 +1,43 @@
+package handler
+
+import (
+	"go-crowdfunding/helper"
+	"go-crowdfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegiserUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "token")
+
+	response := helper.APIResponse("Account has been created", http.StatusOK, "success", formatter)
+	c.JSON(200, response)
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"go-crowdfunding/handler"
+	"go-crowdfunding/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=localhost user=root password=root dbname=crowdfunding port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run(":3080")
+}
